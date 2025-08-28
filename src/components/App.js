b001f37b37fd5908cf278919fe812ee3dac92493
pathania0001 <83996978+pathania0001@users.noreply.github.com>
2025-08-28 14:40:19 +0530

    assignment for submition

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,31 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React,{useState} from "react";
+import Menu from './Menu'
 
+const App = () => {
+  const [filter,setFilter] = useState("all");
+
+  const handleOnClick = (item)=>{
+    setFilter(item);
+  }
+
+  return (
+    <div id="main">
+      <h1 className="text-3xl font-bold text-center mb-6">Our Menu</h1>
+      <div className="flex w-fit mx-auto gap-2 mb-6">
+        {["all","breakfast","lunch","shakes"].map((item,index)=>(
+           <button 
+             key={index}
+             id={`filter-btn-${index+1}`} 
+             onClick={()=>handleOnClick(item)}
+             className="border-none background-none color-filter px-4 py-2 rounded hover:bg-gray-200"
+           >
+             {item[0].toUpperCase() + item.slice(1)}
+           </button>         
+        ))}
+      </div>
+      <Menu filter={filter}/>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import "./styles/App.css"
 
-
-
+const dom = document.getElementById("root");
+ReactDOM.render(<App />, dom);
